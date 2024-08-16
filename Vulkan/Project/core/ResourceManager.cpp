e753e709bc550b138c2040d82a91dc68e5e2d938
Dylan Burgisser <33450259+DijiOfficial@users.noreply.github.com>
2024-08-16 02:28:27 +0200

    added default texture and texture sampler to resourceManager

--- Vulkan/Project/core/ResourceManager.cpp
+++ Vulkan/Project/core/ResourceManager.cpp
@@ -1,4 +1,12 @@
 #include "ResourceManager.h"
+#include "vulkanbase/VulkanBase.h"
+
+void ResourceManager::Init(const VkQueue& graphicsQueue, const VkCommandPool& commandPool, const std::string& data)
+{
+    m_DataPath = data;
+    m_DefaultTexture = LoadTexture(graphicsQueue, commandPool, "default.jpg");
+    CreateTextureSampler();
+}
 
 void ResourceManager::Destroy()
 {
@@ -7,6 +15,7 @@ void ResourceManager::Destroy()
         texture.second->Destroy();
     }
     m_TexturesUPtrUMap.clear();
+    vkDestroySampler(VulkanBase::device, m_TextureSampler, nullptr);
 }
 
 Texture* ResourceManager::LoadTexture(const VkQueue& graphicsQueue, const VkCommandPool& commandPool, const std::string& file)
@@ -19,7 +28,7 @@ Texture* ResourceManager::LoadTexture(const VkQueue& graphicsQueue, const VkComm
     }
 
     Texture* texture = AddTexture(fullPath);
-    texture->CreateTextureImage(graphicsQueue, commandPool, file);
+    texture->CreateTextureImage(graphicsQueue, commandPool, fullPath);
     return texture;
 }
 
@@ -27,4 +36,33 @@ Texture* ResourceManager::AddTexture(const std::string& fullPath)
 {
     m_TexturesUPtrUMap[fullPath] = std::make_unique<Texture>();
     return m_TexturesUPtrUMap[fullPath].get();
+}
+
+void ResourceManager::CreateTextureSampler()
+{
+    VkPhysicalDeviceProperties properties{};
+    vkGetPhysicalDeviceProperties(VulkanBase::physicalDevice, &properties);
+
+    VkSamplerCreateInfo samplerInfo{};
+    samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
+    samplerInfo.magFilter = VK_FILTER_LINEAR;
+    samplerInfo.minFilter = VK_FILTER_LINEAR;
+    samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+    samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+    samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+    samplerInfo.anisotropyEnable = VK_TRUE;
+    samplerInfo.maxAnisotropy = properties.limits.maxSamplerAnisotropy;
+    samplerInfo.borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
+    samplerInfo.unnormalizedCoordinates = VK_FALSE;
+    samplerInfo.compareEnable = VK_FALSE;
+    samplerInfo.compareOp = VK_COMPARE_OP_ALWAYS;
+    samplerInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+    samplerInfo.mipLodBias = 0.0f;
+    samplerInfo.minLod = 0.0f;
+    samplerInfo.maxLod = 0.0f;
+
+    if (vkCreateSampler(VulkanBase::device, &samplerInfo, nullptr, &m_TextureSampler) != VK_SUCCESS)
+    {
+        throw std::runtime_error("failed to create texture sampler!");
+    }
 }
--- Vulkan/Project/core/ResourceManager.h
+++ Vulkan/Project/core/ResourceManager.h
@@ -8,13 +8,20 @@
 class ResourceManager final : public Singleton<ResourceManager>
 {
 public:
-	void Init(const std::string& data) { m_DataPath = data; };
+	void Init(const VkQueue& graphicsQueue, const VkCommandPool& commandPool, const std::string& data);
 	void Destroy();
 
+	VkSampler& GetTextureSampler() { return m_TextureSampler; }
+
 	Texture* LoadTexture(const VkQueue& graphicsQueue, const VkCommandPool& commandPool, const std::string& file);
+	Texture* GetDefaultTexture() const { return m_DefaultTexture; }
 private:
 	std::string m_DataPath;
 
 	std::unordered_map<std::string, std::unique_ptr<Texture>> m_TexturesUPtrUMap;
 	Texture* AddTexture(const std::string& fullPath);
+	Texture* m_DefaultTexture;
+	VkSampler m_TextureSampler;
+
+	void CreateTextureSampler();
 };
