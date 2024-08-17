6a4e005233d29d5bd0d2d1158f387b5699418c89
Dylan Burgisser <33450259+DijiOfficial@users.noreply.github.com>
2024-08-17 03:11:23 +0200

    changed background color

--- Vulkan/Project/vulkanbase/VulkanBase.cpp
+++ Vulkan/Project/vulkanbase/VulkanBase.cpp
@@ -83,7 +83,7 @@ void VulkanBase::DrawFrame(uint32_t imageIndex)
 	renderPassInfo.renderArea.extent = swapChainExtent;
 
 	std::array<VkClearValue, 2> clearValues{};
-	clearValues[0].color = { {0.0f, 0.0f, 0.0f, 1.0f} };
+	clearValues[0].color = { { 0.2f,0.4f,0.6f,1.0f } };
 	clearValues[1].depthStencil = { 1.0f, 0 };
 
 	renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
